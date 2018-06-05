8895a543ad271cf7d995a09b19be7b0901c8bcb0
Fellipe <fellipe@nugo.com.br>
2018-06-05 13:14:09 -0300

    Remove reducer

--- database/index.js
+++ database/index.js
@@ -4,11 +4,9 @@ const DB = require('monk')(config.database)
 const Bill = () => DB.get('bill')
 const Order = () => DB.get('order')
 const OrderDelivery = () => DB.get('orderDelivery')
-const ReduceOrder = () => DB.get('reduceOrder')
 
 module.exports = {
   Bill: Bill,
   Order: Order,
-  OrderDelivery: OrderDelivery,
-  ReduceOrder: ReduceOrder
+  OrderDelivery: OrderDelivery
 }
